4b53ac8ec617904ffeb4b80d533cce88b847f88f
Nuno Marques <nusilmar@gmail.com>
2018-06-06 21:09:53 +0100

    Versão inicial do projecto

---
+++ GAT/GAT/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GAT.MvcApplication" Language="C#" %>
---
+++ GAT/GAT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GAT.Startup))]
+namespace GAT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
